0f187dcbfaa926a5196a32f5cc64cfe08cc4101b
subrasub <s35subra@edu.uwaterloo.ca>
2018-07-22 13:18:08 -0400

    initial commit

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ def dictionary(w):
 		return data[w]
 	elif len(get_close_matches(w, data.keys()))>0:
 		inp = input("Did you mean \"%s\" (y/n)?" % get_close_matches(w, data.keys())[0])
-		if(inp=='y' or inp=='Y'):
+		if inp=='y' or inp=='Y':
 			return data[get_close_matches(w, data.keys())[0]]
+		elif inp == "n" or inp == "N":
+			return "Doesn't exist mate!"
+		else:
+			return "Incorrect input :("
 	else:
 		return "Doesn't exist mate!"
 
 word = input("Enter your word? ")
-print(dictionary(word.lower()))
+ 
+result = dictionary(word)
+
+if type(result) == list:
+	for item in result:
+		print (item)
+else:
+	print (result)
